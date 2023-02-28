16960b41b929f23a578087de19d4da607deaeb07
Boaz Quinones <59629660+AlfredMonty@users.noreply.github.com>
2023-02-27 21:47:16 -0500

    Android Update
    Added proper android code for exiting the app; Cleaned up UI.

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -31,6 +31,16 @@ public class BirdScript : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            //For Android; Take out for Windows build.  
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+                activity.Call<bool>("moveTaskToBack", true);
+            }
+            else
+            {
+                logic.EndApplication();
+            }
             logic.EndApplication();
         }
 
